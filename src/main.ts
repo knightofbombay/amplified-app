2fc90cb365ee21b74fac6acb00257b99120ad6dc
Raj <knightofbombay@yahoo.com>
2020-10-19 11:27:38 -0400

    further updates

--- src/main.ts
+++ src/main.ts
@@ -5,21 +5,21 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 import Amplify from "aws-amplify";
-//import aws_exports from "./aws-exports";
+import aws_exports from "./aws-exports";
 
-const myAppConfig = {
-  // ...
-  'aws_project_region': 'us-east-1',
-  'aws_appsync_graphqlEndpoint': 'https://tbd7dwk4y5fczofwfgyoc42fti.appsync-api.us-east-1.amazonaws.com/graphql',
-  'aws_appsync_region': 'us-east-1',
-  'aws_appsync_authenticationType': 'API_KEY',
-  'aws_appsync_apiKey': 'da2-ouhsq7l2lba2fnczfnwgrhzray',
-  // ...
-}
+// const myAppConfig = {
+//   // ...
+//   'aws_project_region': 'us-east-1',
+//   'aws_appsync_graphqlEndpoint': 'https://tbd7dwk4y5fczofwfgyoc42fti.appsync-api.us-east-1.amazonaws.com/graphql',
+//   'aws_appsync_region': 'us-east-1',
+//   'aws_appsync_authenticationType': 'API_KEY',
+//   'aws_appsync_apiKey': 'da2-ouhsq7l2lba2fnczfnwgrhzray',
+//   // ...
+// }
 
-// Amplify.configure(aws_exports);
+Amplify.configure(aws_exports);
 
-Amplify.configure(myAppConfig);
+// Amplify.configure(myAppConfig);
 
 
 if (environment.production) {
